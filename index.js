979e77d7550af5268554ec309883fba4a9fc3c61
Ridwan Abiola <ricklick262@gmail.com>
2022-01-11 11:45:22 +0100

    feat: cosy room homepage

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const open = document.querySelector('.nav-toggle');
 const nav = document.querySelector('.primary-nav');
 const cover = document.querySelector('.cover');
+const previous = document.querySelector('.arrows > *:first-child');
+const next = document.querySelector('.arrows > *:nth-child(2)');
+const slider = document.querySelector('.hero-top');
+const arrows = document.querySelectorAll('.arrows > *');
 
 open.addEventListener('click', () => {
 	let isOpen = nav.getAttribute('data-open');
@@ -15,3 +19,66 @@ open.addEventListener('click', () => {
 		cover.setAttribute('data-cover', false);
 	}
 });
+
+// console.log(arrows);
+
+arrows.forEach((arrow) => {
+	arrow.addEventListener('click', () => {
+		const offset = arrow.dataset.button === 'next' ? 1 : -1;
+		const slides = document.querySelector('[data-slides]');
+		const textSlides = document.querySelector('[data-text-slides]');
+		const activeSlide = slides.querySelector('[data-active]');
+		const activeTextSlide = textSlides.querySelector('[data-text-active]');
+
+		let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+
+		if (newIndex < 0) newIndex = slides.children.length - 1;
+		if (newIndex >= slides.children.length) newIndex = 0;
+
+		slides.children[newIndex].dataset.active = true;
+		textSlides.children[newIndex].dataset.textActive = true;
+
+		delete activeSlide.dataset.active;
+		delete activeTextSlide.dataset.textActive;
+	});
+});
+
+// previous.addEventListener('click', () => {
+// 	if (slide2 === 'false' && slide3 === 'false') {
+// 		slider.setAttribute('data-slide1', true);
+// 	} else {
+// 		slider.setAttribute('data-slide1', false);
+// 	}
+
+// 	if (slide1 === 'false' && slide3 === 'false') {
+// 		slider.setAttribute('data-slide2', true);
+// 	} else {
+// 		slider.setAttribute('data-slide2', false);
+// 	}
+
+// 	if (slide1 === 'false' && slide2 === 'false') {
+// 		slider.setAttribute('data-slide3', true);
+// 	} else {
+// 		slider.setAttribute('data-slide3', false);
+// 	}
+// });
+
+// next.addEventListener('click', () => {
+// 	if (slide2 === 'false' && slide3 === 'false') {
+// 		slider.setAttribute('data-slide1', true);
+// 	} else {
+// 		slider.setAttribute('data-slide1', false);
+// 	}
+
+// 	if (slide1 === 'false' && slide3 === 'false') {
+// 		slider.setAttribute('data-slide2', true);
+// 	} else {
+// 		slider.setAttribute('data-slide2', false);
+// 	}
+
+// 	if (slide1 === 'false' && slide2 === 'false') {
+// 		slider.setAttribute('data-slide3', true);
+// 	} else {
+// 		slider.setAttribute('data-slide3', false);
+// 	}
+// });
